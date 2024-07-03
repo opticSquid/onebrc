606d07b6fc129e7008f37311e216927d207c9a70
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2024-07-03 22:58:02 +0530

    Initial Commit

---
+++ src/main/java/com/sb/onebrc/ObebrcApplication.java
@@ -0,0 +1,13 @@
+package com.sb.onebrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObebrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObebrcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/obebrc/ObebrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.obebrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObebrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
